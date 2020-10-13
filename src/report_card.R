311724b872c4efd7ad286a62395ad12280c83c6b
JD <dobiasovsky.honza@gmail.com>
2020-10-13 12:25:26 +0200

    Update docstrings

--- src/report_card.R
+++ src/report_card.R
@@ -1,4 +1,13 @@
+# functions to prep tables of data to display as reports
+library(dplyr)
+
 generate_report_card_bydate <- function(data,from,to,doctypes,visibility){
+  # Prepare tibble containing digital library growth by date in table format
+  # @param data: data.frame or tibble with data fetched from index
+  # @param from: string or datetime value for narrowing data from start
+  # @param to: string or datetime value for narrowing data to certain point
+  # @param doctypes: display data for selected document types (see config for possible values)
+  # @param visibility: filter documents by their visibility settings c("private","public")
   data <- data %>% 
     filter(dostupnost %in% visibility) %>%
     filter(fedora.model %in% doctypes) %>%
@@ -16,6 +25,12 @@ generate_report_card_bydate <- function(data,from,to,doctypes,visibility){
 }
 
 generate_report_card_overview <- function(data,from,to,doctypes,visibility){
+  # Sum all documents within selected parameters into single number count and return table.
+  # @param data: data.frame or tibble with data fetched from index
+  # @param from: string or datetime value for narrowing data from start
+  # @param to: string or datetime value for narrowing data to certain point
+  # @param doctypes: display data for selected document types (see config for possible values)
+  # @param visibility: filter documents by their visibility settings c("private","public")
     data <- index %>% 
       filter(dostupnost %in% visibility) %>%
       filter(fedora.model %in% doctypes) %>%
